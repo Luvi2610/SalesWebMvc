f19d239282e26bef533048ee879e8dfec1258fa3
Lucas Ferrarotto <lucas.ferrarotto@gmail.com>
2023-05-18 12:06:00 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //Indica que é uma ação de POST e não de GET
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller )
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); //redireciona para a ação Index
         }
     }
 }
